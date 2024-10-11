772a1115369750e625efca4b36892739700c8e8c
DevyManish <121728309+DevyManish@users.noreply.github.com>
2024-10-12 01:23:45 +0530

    🖼️(assets) : Added Favicon

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -24,6 +24,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+          <head>
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          type="image/x-icon"
+          sizes="16x16"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
