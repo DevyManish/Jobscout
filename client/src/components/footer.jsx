50245dfdaf2c8382c35ef760860a25ed049d1026
DevyManish <121728309+DevyManish@users.noreply.github.com>
2024-10-12 19:16:21 +0530

    🛠️(fix) : Footer for dark theme

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -6,8 +6,10 @@ export function Footer({ className }) {
   return (
     <footer className={cn(className)}>
       <div className="container flex px-3 pt-10 md:pt-36 md:px-40 items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex items-center gap-4 px-4 md:flex-row md:gap-2 md:px-0">
-        <Image src="/logo.png" alt="Logo" width={100} height={65} />
+        <div className="flex items-center gap-4 px-4 py-3 md:flex-row md:gap-2 md:px-0">
+        {/* <Image src="/logo.png" alt="Logo" width={100} height={65} /> */}
+        <Image src="/logo.png" alt="Logo" className="dark:hidden" width={30} height={70} />
+        <Image src="/blogo.png" alt="Logo" className="hidden dark:block" width={30} height={70} />
         <p className="text-center text-sm leading-loose md:text-left">
             Built by{" "}
             <a
